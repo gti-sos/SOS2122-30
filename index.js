2e64860eaafd95b0b8f957dd4b94a978a1547a4d
usuario <correo>
2022-03-11 14:06:10 +0100

    added dinamic route /cool and index.html #7

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const cool = require("cool-ascii-faces");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
-    console.log("Requested / route");
+app.use("/", express.static('public'));
+
+app.get("/cool",(req,res)=>{
+    console.log("Requested /cool route");
     res.send("<html><body><h1>"+cool()+"</h1></body></html>");
+});
+
+
+app.get("/time",(req,res)=>{
+    console.log("Requested /time route");
+    res.send("<html><body><h1>"+new Date()+"</h1></body></html>");
 });
 
 app.listen(port, () => {
