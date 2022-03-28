7528ac62fcc3596526e37a4a554936ad99a27a4e
Jaiquisic23 <jaimequinterosicre@gmail.com>
2022-03-28 13:53:25 +0200

    index.js actualizado

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ const s = "/stsatellites-stats";
 //URL Sergio
 const url_sergio = "/cryptocoin-stats";
 
+//URL Jaime
+const url_jaime = "/technology_devices_stats";
+
 // https://documenter.getpostman.com/view/19481690/UVyn1ycR
 
 app.use(bodyParser.json());
@@ -70,49 +73,51 @@ var satellites_2 = satellites;
 //Parte de Jaime
 var technology_devices_stats = [
     {
-        country: "EEUU",
-        year: 2019,
-        tdwasted: 6918,
-        mpdisuse: 220,
-        mpreused: 74
+        "country": "EEUU",
+        "year": 2019,
+        "tdwasted": 6918,
+        "mpdisuse": 220,
+        "mpreused": 74
     },
     {
-        country: "Spain",
-        year: 2019,
-        tdwasted: 1450,
-        mpdisuse: 45.4,
-        mpreused: 89
+        "country": "Spain",
+        "year": 2019,
+        "tdwasted": 1450,
+        "mpdisuse": 45.4,
+        "mpreused": 89
     },
     {
-        country: "UK",
-        year: 2019,
-        tdwasted: 2695,
-        mpdisuse: 83.1,
-        mpreused: 83
+        "country": "UK",
+        "year": 2019,
+        "tdwasted": 2695,
+        "mpdisuse": 83.1,
+        "mpreused": 83
     },
     {
-        country: "Germany",
-        year: 2019,
-        tdwasted: 2549,
-        mpdisuse: 84.7,
-        mpreused: 84
+        "country": "Germany",
+        "year": 2019,
+        "tdwasted": 2549,
+        "mpdisuse": 84.7,
+        "mpreused": 84
     },
     {
-        country: "France",
-        year: 2019,
-        tdwasted: 2129,
-        mpdisuse: 66.4,
-        mpreused: 76
+        "country": "France",
+        "year": 2019,
+        "tdwasted": 2129,
+        "mpdisuse": 66.4,
+        "mpreused": 76
     },
     {
-        country: "Canada",
-        year: 2019,
-        tdwasted: 739,
-        mpdisuse: 22.5,
-        mpreused: 70
+        "country": "Canada",
+        "year": 2019,
+        "tdwasted": 739,
+        "mpdisuse": 22.5,
+        "mpreused": 70
     }
 
 ];
+
+var technology_devices_stats2 = technology_devices_stats;
 
 // Parte de Sergio
 var cryptocoinstats = [
@@ -494,31 +499,81 @@ app.put(BASE_API_URL + url_sergio + "/:country/:year", (req,res)=>{
 
 
 //--------------------------------------------------------------
+//PARTE JAIME
+
+const jaime_URL_API = "https://documenter.getpostman.com/view/19584746/UVyoUwqe";
+
+app.get(BASE_API_URL + url_jaime +"/docs", (req,res) => {
+    res.redirect(jaime_URL_API);
+});
+
+//LOAD INICIAL
+app.get(BASE_API_URL + jaime_URL_API + "/loadInitialData", (req,res)=>{
+    var td =  technology_devices_stats.length;
+    if (td == 0){
+        technology_devices_stats = technology_devices_stats2;
+        res.redirect(BASE_API_URL + jaime_URL_API);
+    } else{
+        res.sendStatus(409, "Conflict");
+    }
+});
 
 
 //GET 
 
 //Conjunto
-
-
-app.get(BASE_API_URL +"/technology_devices_stats",(req,res)=>{
+app.get(BASE_API_URL + url_jaime,(req,res)=>{
     res.send(JSON.stringify(technology_devices_stats,null,2));
 
 });
 
-
-
 //Elemento
+app.get(BASE_API_URL + url_jaime + "/country/:year", (req, res)=>{
+    var tdCountry = req.params.country;
+    var tdYear = req.params.year;
+    
+    
+    filteredTD = technology_devices_stats.filter((technology_devices_stats)=>{
+        return(technology_devices_stats.country == tdCountry && technology_devices_stats.year == tdYear);
+    });
+    if(filteredTD == 0){
+        res.sendStatus(404, "NOT FOUND");
+    }else{
+        res.status(200);
+        res.send(JSON.stringify(filteredTD[0],null,2)); 
+    }
+});
 
 
 
 //POST
 
+//Conjunto
+app.post(BASE_API_URL + url_jaime, (req,res)=>{
+    tdBody = req.body;
+    td_Country = req.body.country;
+    td_Year = req.body.year;
+    if(!tdBody.country || !tdBody.year || !tdBody.tdwasted || !tdBody.mpdisuse || !tdBody.mpreused){
+        console.log("Data is missing or incorrect");
+        return res.sendStatus(400);
+    }else {
+        for(var i = 0; i < technology_devices_stats.length; i++){
+            if(technology_devices_stats[i].country == td_Country && technology_devices_stats[i].year == td_Year){
+                return res.sendStatus(409);
+            }
+        }
+        technology_devices_stats.push(tdBody);
+        res.sendStatus(201, "CREATED");
 
-app.post(BASE_API_URL +"/technology_devices_stats",(req,res)=>{
-    technology_devices_stats.push(req.body);
+    }
+});
 
-    res.sendStatus(201, "CREATED");
+
+
+//Elemento
+app.post(BASE_API_URL + url_jaime + "/country/:year",(req,res)=>{
+
+    res.sendStatus(405, "Method not allowed");
 
 });
 
@@ -527,15 +582,64 @@ app.post(BASE_API_URL +"/technology_devices_stats",(req,res)=>{
 //DELETE
 
 //Conjunto
-
+app.delete(BASE_API_URL + url_jaime, (req, res)=>{
+    technology_devices_stats = [];
+    res.sendStatus(200, "OK");
+});
 
 
 //Elemento
+app.delete(BASE_API_URL + url_jaime+ "/:country/:year", (req, res)=>{
+    var tdCountry = req.params.country;
+    var tdYear = req.params.year;
+    var original = technology_devices_stats;
 
+    technology_devices_stats = technology_devices_stats.filter((technology_devices_stats)=>{
+        return (technology_devices_stats.country != tdCountry || technology_devices_stats.year != tdYear);
+    });
+
+    if(technology_devices_stats.length == original.length){
+        res.sendStatus(404);
+    } else {
+        res.sendStatus(200, "OK");
+    }
+    
+    
+});
 
 
 //PUT
 
+//CONJUNTO
+app.put(BASE_API_URL + url_jaime, (req,res)=>{
+    res.sendStatus(405,"Unabe to PUT a resource list");
+});
+
+//ELEMENTO
+
+app.put(BASE_API_URL + url_jaime + "/:country/:year", (req,res)=>{
+    var td_params = req.params;         
+    var td_body = req.body;            
+
+    var tdCountry = req.params.country;
+    var tdYear = req.params.year;
+
+    if(!td_body.country || !td_body.year || !td_body.tdwasted || !td_body.mpdisuse || !td_body.mpreused){
+        console.log("Data is missing or incorrect");
+        return res.sendStatus(400);
+        
+
+    } else {
+        for(var i = 0; i < technology_devices_stats.length; i++){
+            if(technology_devices_stats[i].country == tdCountry && technology_devices_stats[i].year == tdYear){
+                technology_devices_stats[i] = td_body;
+                console.log(technology_devices_stats[i].body);
+                break;
+            }
+        }
+        res.sendStatus(200, "OK");
+    }
+});
 
 //-----------------------------------------------------------------------------
 
