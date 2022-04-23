9758c7d74ecb8b88526e5ca30a12f9d0ecb42700
usuario <correo>
2022-04-23 20:41:39 +0200

    Load y Edit

--- src/front/servelgar1Front/cryptocoinEdit.svelte
+++ src/front/servelgar1Front/cryptocoinEdit.svelte
@@ -1,10 +1,131 @@
 
 <script>
-	import Cryptocoin_table from './cryptocoinTable.svelte';
+	  import {onMount} from "svelte";
+    import {pop} from "svelte-spa-router";
+    import Alert from 'sveltestrap/src/Alert.svelte';
+    import Table from "sveltestrap/src/Table.svelte";
+    import Button from "sveltestrap/src/Button.svelte";
+
+  export let params = {};
+  let cc = {};
+	let upCountry = "";
+	let upYear = "";
+	let upElectr = "";
+	let upDemand = "";
+	let upMining = "";
+  let errorMsg = "";
+ 	let okMsg = "";
+	let visible = false;
+	let visibleOk = false;
+
+
+  onMount(getCrypto);
+
+
+  async function getCrypto() {
+        console.log("Fetching data..." + params.country + " " + params.year);
+        const res = await fetch("/api/v2/cryptocoin-stats" + params.country +"/" + params.year);
+        if (res.ok) {
+            console.log("Ok:");
+            const json = await res.json();
+            cc = json;
+            upCountry = cc.country;
+            upYear = parseInt(cc.year);
+            upElectr = parseInt(cc.ccelectr);
+            upDemand = parseInt(cc.ccdemand);
+            upMining = parseInt(cc.ccmining);
+            console.log(JSON.stringify(cc));
+            console.log("Received data.");
+        } else {
+            if(res.status === 404){
+                    errorMsg = `No existe dato con país: ${params.country} y año: ${params.year}`;
+                    console.log("ERROR!" + errorMsg);
+              visibleOk=false;
+              visible=true;
+		      	} else if (res.status === 500) {
+        		    errorMsg = "No se han podido acceder a la base de datos";
+      	  	}
+			
+      		console.log("ERROR!" + errorMsg);
+        }
+    }
+
+
+    async function updateCC() {
+        console.log("Updating data..." );
+        const res = await fetch("/api/v2/cryptocoin-stats" + upCountry +"/" + upYear, {
+            method: "PUT",
+            body: JSON.stringify({
+              country : upCountry,
+              year : parseInt(upYear),
+			        ccelectr : parseInt(upElectr),
+              ccdemand : parseInt(upDemand),
+	 		        ccmining : parseInt(upMining)
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(function (res) {
+          visible = true;
+          getCrypto();
+            if(res.status == 200 || res.status == 201){
+              okMsg = "Actualización correcta";
+              visibleOk=true;
+              }else{
+                if(res.status === 404){
+                  errorMsg ="El dato solicitado no existe";
+                  visibleOk=false;
+                }
+              }
+              console.log("ERROR!" + errorMsg);
+          });
+    }
+
+
+
 </script>
 
 <main>
-	<h1>Cryptocoin stats</h1>
-	 
-    <Cryptocoin_table />
+  <h3>Editar campos <strong>{params.country}</strong><strong>{params.year}</strong></h3>
+  <Table bordered>
+      <thead>
+          <div>
+              <Alert color="danger" isOpen={visible} toggle={() => (visible = false)}>
+                  {#if errorMsg}
+                      <p>ERROR: {errorMsg}</p>
+                     {/if}
+              </Alert>
+              <Alert color="success" isOpen={visibleOk} toggle={() => (visibleOk = false)}>
+                  {#if okMsg}
+                      <p>Correcto: {okMsg}</p>
+                      {/if}
+              </Alert>
+          </div>
+      
+      
+          <tr>
+              <th>País</th>
+              <th>Año</th>
+              <th>Consumo electrico</th>
+              <th>Demanda electrica</th>
+              <th>Porcentaje de mining</th>
+          </tr>
+      </thead>
+      
+      <tbody>
+      
+          <tr>
+              <td>{upCountry}</td>
+              <td>{upYear }</td>
+              <td><input type=number bind:value="{upElectr}"></td>
+              <td><input type=number bind:value="{upDemand}"></td>
+              <td><input type=number bind:value="{upMining}"></td>
+              <td><Button on:click={updateCC}>Actualizar</Button></td>
+              
+          </tr>
+      </tbody>
+  </Table>
+  
+  
+  <Button outline color="secondary" on:click="{pop}">Volver</Button>
   </main>
--- src/front/servelgar1Front/cryptocoinTable.svelte
+++ src/front/servelgar1Front/cryptocoinTable.svelte
@@ -2,7 +2,7 @@
     import {onMount} from 'svelte';
 	import Table from "sveltestrap/src/Table.svelte";
 	import Button from "sveltestrap/src/Button.svelte";
-
+	import {Pagination, PaginationItem, PaginationLink} from "sveltestrap";
 	
 
 
@@ -21,14 +21,51 @@
 	let visible = false;
 	let visibleOk = false;
 	let p1;
+	let totaldata=12;
 
+	// Variables para la paginación
+	let c_offset = 0;
+    let offset = 0;
+    let limit = 10;
+    let c_page = 1;
+    let lastPage = 1;
+    let total = 0;
+
+
+
+
+	function range(size, start = 0) {
+      return [...Array(size).keys()].map((i) => i + start);
+	}
+
+	function cambiapag(page, offset) {
+      
+      lastPage = Math.ceil(total/10);
+      console.log("Last page = " + lastPage);
+      if (page !== c_page) {
+        c_offset = offset;
+        c_page = page;
+        getReg();
+		getOlimpic1();
+      }
+    }
+
+
+	async function paginacion() {
+      const data = await fetch("/api/v2/cryptocoin-stats");
+      if (data.status == 200 || res.status == 201 ) {
+        const json = await data.json();
+        total = json.length;
+        cambiapag(c_page, c_offset);
+      } 
+    }
 
 	//let loading = true;
 	onMount(getCrypto);
 	async function getCrypto(){
 		console.log("Fetching stats ... ");
 		const res =  await fetch("/api/v2/cryptocoin-stats");
-		if(res.ok){
+		if(res.status == 200 || res.status == 201){
 			const data =await res.json();
 			cc = data;
 			p1 = cc[0];
@@ -36,6 +73,30 @@
 		}
 		
 	}
+
+	async function getOlimpic1() {
+    	console.log("Fetching data...");
+   		const res = await fetch("/api/v2/cryptocoin-stats" + "?limit=" + limit + "&offset=" + c_offset);
+		
+        if(res.status == 200 || res.status == 201){
+			console.log("Ok.");
+			const json = await res.json();
+			cc = json;
+			console.log(`We have ${cc.length} olimpic.`);
+			for(let i=0; i<cc.length ; i++){
+				let c = [];
+				let y = cc[i].year;
+				if(y > yFrom && y<yTo){
+					c.push(y);
+					cc = c;
+				}
+			}
+			paginacion();
+		}else{
+			console.log("Error");
+			
+		}
+  	}
 
 	async function insertCryptocoin(){
 		console.log("Inserting cryptocoin stat...." +JSON.stringify(newCC));
@@ -50,14 +111,15 @@
 				
 				if(res.status == 200 ||res.status == 201){
 					getCrypto();
+					getOlimpic1();
 					okMsg = "Recurso añadido correctamente";
 					visibleOk=true;
 					visible=false;
 					window.alert(okMsg);
 				
 				}else{
-					if(res.status == 404){
-						errorMsg ="El dato no ha sido encontrado";
+					if(res.status == 409){
+						errorMsg ="Ya existe ese dato";
 						visibleOk=false;
 						visible=true;
 						window.alert("ERROR!" + errorMsg);
@@ -77,18 +139,26 @@
 			await fetch("/api/v2/cryptocoin-stats"+ "/" + countryDel + "/" + yearDel, {
             method: "DELETE"
         }).then(function (res) {
-            visible = true;
-            getCrypto();      
-            if (res.status==200) {
+            getCrypto();
+			getOlimpic1();      
+            if (res.status==200 || res.status == 201) {
                 errorMsg = "Recurso "+countryDel +" " + yearDel+ " borrado correctamente";
-                console.log("Deleted " + countryDel);            
+                console.log("Deleted " + countryDel);
+				visibleOk = true;
+				visible = false;  
+				           
             } else if (res.status==404) {
                 errorMsg = "No se ha encontrado el objeto " + countryDel;
-                console.log("Resource NOT FOUND");            
+                console.log("Resource NOT FOUND");
+				visibleOk = false;
+				visible = true;              
             } else {
                 errorMsg= res.status + ": " + "No se pudo borrar el recurso";
                 console.log("ERROR!");
-            }      
+				visibleOk = false;
+				visible = true;  
+            }
+			     
         });
    	 }
 
@@ -98,7 +168,8 @@
 								method: "DELETE"	
 							}).then( function (res) {
 							if(res.ok){
-								getRegistrations();
+								getCrypto();
+								getOlimpic1();
 								okMsg = "Todos los datos se han eliminado";
 								visibleOk=true;
 								visible=false;
@@ -113,12 +184,37 @@
 
 	async function loadCrypto(){
         console.log("Loading entries....");
-        const res = await fetch("/api/v2/cryptocoins-stats/loadInitialData",
-			{
-				method: "GET"
-			}).then(function (res){
-				getCrypto();
-			});
+        const msg = await fetch("/api/v2/cryptocoin-stats/loadInitialData").then(function(res){
+			visible = true;
+			if(res.status == 200 || res.status == 201){
+				console.log("200");
+				errorMsg = "Objetos cargados correctamente";
+			} else if (res.status == 400){
+				errorMsg = "Ha ocurrido un fallo";
+				console.log("BAD REQUEST");
+			} else {
+				errorMSG= res.status + ": " + res.statusText;
+                console.log("ERROR!");
+			}
+		});
+
+		const elements = await fetch("/api/v2/cryptocoin-stats/loadInitialData"); //datos cargados
+        const jsonElements = await elements.json();
+        c_page = 1;
+        totalObj = jsonElements.length;
+        console.log("ELEMENTOS: "+ totalObj);
+        const res = await fetch("/api/v2/cryptocoin-stats?limit=10&offset=1"); //datos mostrados
+        if (res.ok) {
+            console.log("Ok:");
+            const json = await res.json();
+            cc = json;
+			getCrypto();
+            console.log("Loading "+poverty.length+" objects");
+            console.log("Received " + poverty.length + " cryptocoin stat.");
+        } else {
+            console.log("ERROR!");
+        }
+
     }
 </script>
 
@@ -130,6 +226,13 @@
 	{:then cc} 
 	<Table bordered>
 		<thead>
+
+			<tr>
+				<td><Button outline color="success" on:click={loadCrypto}> Cargar datos</Button></td>
+				<td><Button outline color="danger" on:click={delCrypto}>Borrar todo</Button></td>
+			</tr>
+			
+
 			<tr>
 				<th>Country</th>
 				<th>year</th>
@@ -160,24 +263,32 @@
 				<td>{cc.ccmining}</td>
 				<td><Button outline color="warning" on:click={function (){
 					window.location.href = `/#/ccTable/${cc.country}/${cc.year}`
-				}}>
-					Editar
-				</Button>
+				}}>Editar</Button>
 				<td><Button outline color="danger" on:click={delCryptoUnico(cc.country,cc.year)}>
 					Borrar
 				</Button>
 				</td>
 			</tr>
 			{/each}
-			<tr>
-				<td><Button outline color="success" on:click={loadCrypto}>
-					Cargar datos
-				</Button></td>
-				<td><Button outline color="danger" on:click={delCrypto}>
-					Borrar todo
-				</Button></td>
-			</tr>
+			
 		</tbody>
+		<div>
+			<Pagination ariaLabel="Web pagination">
+				<PaginationItem class = {c_page === 1 ? "enable" : ""}>
+					  <PaginationLink previous href="#/ccTable" on:click={() => cambiapag(c_page - 1, c_offset - 10)}/>
+				</PaginationItem>
+				{#each range(lastPage, 1) as page}
+					  <PaginationItem class = {c_page === page ? "active" : ""}>
+						<PaginationLink previous href="#/ccTable" on:click={() => cambiapag(page, (page - 1) * 10)}>
+							{page}
+						</PaginationLink>
+					  </PaginationItem>
+				{/each}
+				<PaginationItem class = {c_page === lastPage ? "disabled" : ""}>
+					  <PaginationLink next href="#/ccTable" on:click={() => cambiapag(c_page + 1, c_offset + 10)}/>
+				</PaginationItem>
+			  </Pagination>
+		</div>
 	</Table>
 	{/await}
 </main>
