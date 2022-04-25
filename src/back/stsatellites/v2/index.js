a85fe9d4d0072dc8d9e948c29d11a561e3c82b34
J AY <javi.ayanez@gmail.com>
2022-04-25 20:09:41 +0200

    fix st

--- src/back/stsatellites/v2/index.js
+++ src/back/stsatellites/v2/index.js
@@ -192,6 +192,34 @@ module.exports = (app) => {
 
     });
     
+    //Elemento
+    app.get(BASE_API_URL + url_javier + "/:country/:year", (req, res)=>{
+        var satYear = parseInt(req.params.year);
+        var satCountry = req.params.country;
+        var satQ = req.params.quarter;
+
+        db.find({country : satCountry, year: satYear}, {_id:0}, function(err,data){
+            console.log("0");
+            if(err){
+                console.log("1");
+                console.error("ERROR GET: "+ err);
+                res.sendStatus(500, "Internal Server Error");
+            }else {
+                console.log("2");
+                if(data.length != 0){
+                    console.log("3");
+                    res.send(JSON.stringify(data,null,2));
+                    res.status(200);
+                }else{
+                    console.log("4");
+                    console.error("Data not found");
+                    res.status(404);
+                    res.send("Data not found");
+                }
+            }
+        });
+
+    });
 
     //POST
 
